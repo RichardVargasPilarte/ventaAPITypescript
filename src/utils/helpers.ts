f602379a23e6a4966be72051cbdfe2c79231486c
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-09-18 11:28:00 -0600

    eliminacion del password al momento de la creacion

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,17 +6,44 @@ const prisma = new PrismaClient();
 
 // Función para transformar el campo id a uid
 export const transformToUid = (usuario: any) => {
-    const { id, ...data } = usuario;
+    const { id, password, ...data } = usuario;
     return { uid: id, ...data };
 };
 
+type CampoBusqueda = 'nombre' | 'username' | 'email';
+
 // Función para verificar la existencia de un campo en la base de datos
-export const verificarExistencia = async (campo: 'nombre' | 'username' | 'email', valor: string, prismaClient: PrismaClient) => {
-    const where = { [campo]: valor };
-    return await prismaClient.usuario.findUnique({
-        where: where as any
-    });
+export const verificarExistencia = async (campo: CampoBusqueda, valor: string, prismaClient: PrismaClient) => {
+    // Validar que el valor no esté vacío
+    if (!valor || valor.trim() === '') {
+        throw new Error('El valor para la búsqueda no puede ser nulo o vacío');
+    }
+
+    // Validar que el campo sea uno de los permitidos
+    if (!['nombre', 'username', 'email'].includes(campo)) {
+        throw new Error('Campo no válido para la búsqueda');
+    }
+
+    // Construir el objeto where dinámicamente
+    const where = { [campo]: valor } as Record<CampoBusqueda, string>;
+
+    try {
+        // Realizar la consulta en Prisma
+        const resultado = await prismaClient.usuario.findUnique({
+            where,
+        });
+
+        return resultado;
+    } catch (error: unknown) {
+        // Manejar errores de la consulta
+        if (error instanceof Error) {
+            throw new Error(`Error al verificar existencia: ${error.message}`);
+        } else {
+            throw new Error('Error desconocido al verificar existencia');
+        }
+    }
 };
+
 
 // Función para actualizar el estado del usuario
 export const actualizarEstadoUsuario = async (usuarioId: string, estado: number, prismaClient: PrismaClient) => {
