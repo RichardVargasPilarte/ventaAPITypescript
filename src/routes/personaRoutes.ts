cf49dc47855f40db8bc041e540914c2a14ea6ab0
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-09-20 07:53:30 -0600

    creacion de ruta para persona

--- src/routes/personaRoutes.ts
+++ src/routes/personaRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as personaController from '../controllers/personaController';
 import { body } from 'express-validator';
 
-import { validarCampos } from '../middlewares/validarCampos ';
+import { validarCampos } from '../middlewares/validarCampos';
 
 const router = Router();
 
