ac6373d7c1f59a280abea99806669e3ed3d221c0
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-09-20 07:54:11 -0600

    declaracion de nuevas rutas en index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-//import userRoutes from './routes/usuarioRoutes';
+import usuarioRoutes from './routes/usuarioRoutes';
 import articuloRoutes from './routes/articuloRoutes';
 import categoriaRoutes from './routes/categoriaRoutes';
+import personaRoutes from './routes/personaRoutes';
+import ingresoRoutes from './routes/ingresoRoutes';
+import loginRoutes from './routes/loginRoutes'
+
 dotenv.config();
 
 const app = express();
@@ -13,9 +17,12 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas
-//app.use('/api/usuarios', userRoutes);
+app.use('/api/usuario', usuarioRoutes);
 app.use('/api/articulo', articuloRoutes);
 app.use('/api/categoria', categoriaRoutes);
+app.use('/api/persona', personaRoutes);
+app.use('/api/ingreso', ingresoRoutes);
+app.use('/api/login', loginRoutes);
 
 app.listen(process.env.APP_PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.APP_PORT || 3000}`);
