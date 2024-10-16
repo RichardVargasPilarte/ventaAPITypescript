2f710104e900a21eb303fd2522c2f506e40b51f6
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-10-16 09:14:57 -0600

    implementacion del middleware validarJWT en los diferentes puntos finales

--- src/routes/articuloRoutes.ts
+++ src/routes/articuloRoutes.ts
@@ -2,15 +2,17 @@ import { Router } from 'express';
 import * as articuloController from '../controllers/articuloController';
 import { body } from 'express-validator';
 
+import { validarJWT } from '../middlewares/validarJWT';
 import { validarCampos } from '../middlewares/validarCampos';
 
 const router = Router();
 
-router.get('/', articuloController.obtenerArticulos);
-router.get('/:id', articuloController.obtenerArticuloId);
-router.get('/:codigo', articuloController.obtenerArticuloPorCodigo);
+router.get('/', validarJWT, articuloController.obtenerArticulos);
+router.get('/:id', validarJWT, articuloController.obtenerArticuloId);
+router.get('/:codigo', validarJWT, articuloController.obtenerArticuloPorCodigo);
 
 router.post('/', [
+    validarJWT,
     body('categoria_id').notEmpty().withMessage('El campo categoria_id es obligatorio'),
     body('nombre').notEmpty().withMessage('El campo nombre es obligatorio').isLength({ max: 50 }).withMessage('El nombre no puede tener más de 50 caracteres'),
     body('precio_venta').isFloat().withMessage('El campo precio_venta es obligatorio y debe ser un número'),
@@ -21,6 +23,7 @@ router.post('/', [
 ], validarCampos, articuloController.crearArticulo);
 
 router.put('/:id', [
+    validarJWT,
     body('categoria_id').optional().notEmpty().withMessage('El campo categoria_id no puede estar vacío'),
     body('nombre').optional().isLength({ max: 50 }).withMessage('El nombre no puede tener más de 50 caracteres'),
     body('precio_venta').optional().isFloat().withMessage('El campo precio_venta debe ser un número'),
@@ -30,8 +33,8 @@ router.put('/:id', [
     body('descripcion').optional().isLength({ max: 255 }).withMessage('El campo descripcion, si se proporciona, no puede tener más de 255 caracteres'),
 ], validarCampos, articuloController.actualizarArticulo);
 
-router.delete('/:id', articuloController.eliminarArticulo);
-router.put('/:id/activar', articuloController.activarArticulo);
-router.put('/:id/desactivar', articuloController.desactivarArticulo);
+router.delete('/:id', validarJWT, articuloController.eliminarArticulo);
+router.put('/:id/activar', validarJWT, articuloController.activarArticulo);
+router.put('/:id/desactivar', validarJWT, articuloController.desactivarArticulo);
 
 export default router;
